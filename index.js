5c1953ea69b9c3e8773a2375fb46bbe243426713
JfJubaer <skk5727@gmail.com>
2023-01-13 17:33:08 +0600

    mongo

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 4000 || process.env.PORT;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 // middle wire
 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j88am2v.mongodb.net/?retryWrites=true&w=majority`;
+
+
 
 
 app.get('/', (req, res) => {
